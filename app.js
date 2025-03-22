e6b83741c92aa756c7319035db54b6c40daf3749
Captain-Fahd <omarfahd06@gmail.com>
2025-03-22 18:45:49 +1100

    API implemented A working weather API fetch request was implemented. The result displayed in a header tag for testing purposes.

--- app.js
+++ app.js
@@ -1,10 +1,46 @@
 const input = document.querySelector(".weather-input");
 const btn = document.querySelector(".submit-btn");
 const main = document.querySelector(".main");
+const resultDoc = document.querySelector(".result");
+let currTemp = '';
 
 
-btn.addEventListener("click", (event) => {
+btn.addEventListener("click", async (event) => {
+    if (currTemp !== '') {
+        currTemp = '';
+    }
 
-    main.style.width = "800px";
+    main.style.transition = "all 1s";
+    main.style.width = "1000px";
+    main.style.height = "400px";
+
+    currTemp = await getWeather(input.value);
+
+    tempVal = document.createElement("h3");
+    resultDoc.appendChild(tempVal);
+    tempVal.innerText = currTemp;
 
 })
+//link : https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=us&key=PD9PGCUBFQJQL4RVTS6A9FP7Q&
+//     contentType=json
+
+async function getWeather(city) {
+    try {
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/
+        ${city}?unitGroup=metric&key=PD9PGCUBFQJQL4RVTS6A9FP7Q&contentType=json`);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error: ${response.status}`);
+        }
+
+        const data = await response.json();
+        const temp = data.currentConditions.temp;
+
+        return temp;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+
+
+}
