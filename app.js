e081598cbb73bff7b1850e90560d6dc74d40cfa9
Captain-Fahd <omarfahd06@gmail.com>
2025-03-22 12:00:51 +1100

    Slight Changes -Tested JS connection - Remodified the design to include transitions for the button design, and to the main body on-click.

---
+++ app.js
@@ -0,0 +1,10 @@
+const input = document.querySelector(".weather-input");
+const btn = document.querySelector(".submit-btn");
+const main = document.querySelector(".main");
+
+
+btn.addEventListener("click", (event) => {
+
+    main.style.width = "800px";
+
+})
