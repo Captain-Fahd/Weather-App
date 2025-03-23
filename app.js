82e3d31d5264c31899adc203c52b24723e5e1edf
Captain-Fahd <omarfahd06@gmail.com>
2025-03-23 18:06:50 +1100

    App is functional The app retrieves the requested city, and displays the temperature in that respective city. It also deletes previous queries in order to avoid congestion and confusion.

--- app.js
+++ app.js
@@ -2,23 +2,27 @@ const input = document.querySelector(".weather-input");
 const btn = document.querySelector(".submit-btn");
 const main = document.querySelector(".main");
 const resultDoc = document.querySelector(".result");
-let currTemp = '';
+let tempVal = '';
 
 
-btn.addEventListener("click", async (event) => {
-    if (currTemp !== '') {
-        currTemp = '';
+btn.addEventListener("click", async () => {
+    if (tempVal !== '') {
+        tempVal = '';
+        resultDoc.textContent = '';
     }
 
     main.style.transition = "all 1s";
     main.style.width = "1000px";
     main.style.height = "400px";
 
-    currTemp = await getWeather(input.value);
+    tempVal = await getWeather(input.value);
+    //Creating and assigning the Heading for the Temperature
+   const weatherHeading = document.createElement("p");
+   resultDoc.appendChild(weatherHeading);
+   weatherHeading.textContent = `The Temperature in ${input.value} is currently:${tempVal} \u00B0 C `;
 
-    tempVal = document.createElement("h3");
-    resultDoc.appendChild(tempVal);
-    tempVal.innerText = currTemp;
+   weatherHeading.style.fontSize = "40px";
+
 
 })
 //link : https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=us&key=PD9PGCUBFQJQL4RVTS6A9FP7Q&
@@ -34,9 +38,7 @@ async function getWeather(city) {
         }
 
         const data = await response.json();
-        const temp = data.currentConditions.temp;
-
-        return temp;
+        return data.currentConditions.temp;
     } catch (error) {
         console.error(error);
         throw error;
